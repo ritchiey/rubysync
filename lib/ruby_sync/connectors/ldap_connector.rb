96fdf488e79d58a8c0d2f0c2c79ca3c48f2a927c
Ritchie Young <ritchiey@gmail.com>
2007-11-19 02:45:20 +0000

    Updated ldap connector to open/close ldap connections less frequently
    git-svn-id: http://rubysync.rubyforge.org/svn/trunk@81 0ca38906-61f3-41d5-b8b3-647020b6ede6

--- lib/ruby_sync/connectors/ldap_connector.rb
+++ lib/ruby_sync/connectors/ldap_connector.rb
@@ -3,19 +3,26 @@
 #  Copyright (c) 2007 Ritchie Young. All rights reserved.
 #
 # This file is part of RubySync.
-# 
-# RubySync is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-# 
-# RubySync is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along with RubySync; if not, write to the
-# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+#
+# RubySync is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2 of
+the License, or (at your option) any later version.
+#
+# RubySync is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+along with RubySync; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA
 
 
 lib_path = File.dirname(__FILE__) + '/..'
-$:.unshift lib_path unless $:.include?(lib_path) || $:.include?(File.expand_path(lib_path))
+$:.unshift lib_path unless $:.include?(lib_path) ||
+$:.include?(File.expand_path(lib_path))
 
 require 'ruby_sync'
 require 'net/ldif'
@@ -25,159 +32,171 @@ require 'net/ldap'
 
 
 class Net::LDAP::Entry
-  def to_hash
-    return @myhash.dup
-  end
+ def to_hash
+   return @myhash.dup
+ end
 end
 
 module RubySync::Connectors
-  class LdapConnector < RubySync::Connectors::BaseConnector
-    
-    option  :host,
-    :port,
-    :bind_method,
-    :username,
-    :password,
-    :search_filter,
-    :search_base,
-    :association_attribute # name of the attribute in which to store the association key(s)
-            
-    association_attribute 'RubySyncAssociation'
-    bind_method           :simple
-    host                  'localhost'
-    port                  389
-    search_filter         "cn=*"
+ class LdapConnector < RubySync::Connectors::BaseConnector
 
-    def initialize options={}
-      super options
-    end
+   option  :host,
+   :port,
+   :bind_method,
+   :username,
+   :password,
+   :search_filter,
+   :search_base,
+   :association_attribute # name of the attribute in which to store
+the association key(s)
 
+   association_attribute 'RubySyncAssociation'
+   bind_method           :simple
+   host                  'localhost'
+   port                  389
+   search_filter         "cn=*"
 
-    def started
-      #TODO: If vault, check the schema to make sure that the association_attribute is there
-    end
-    
-
-    def each_entry
-      Net::LDAP.open(:host=>host, :port=>port, :auth=>auth) do |ldap|
-        ldap.search :base => search_base, :filter => search_filter, :return_result => false do |ldap_entry|
-          yield ldap_entry.dn, to_entry(ldap_entry)
-        end
-      end
-    end
-    
-    # Runs the query specified by the config, gets the objectclass of the first
-    # returned object and returns a list of its allowed attributes
-    def self.fields
-      log.warn "Fields method not yet implemented for LDAP - Sorry."
-      log.warn "Returning a likely sample set."
-      %w{ cn givenName sn }
-    end
-    
+   def initialize options={}
+     super options
+   end
 
 
-    def self.sample_config
-      return <<END
-      
-   host           'localhost'
-   port            389
-   username       'cn=Manager,dc=my-domain,dc=com'
-   password       'secret'
-   search_filter  "cn=*"
-   search_base    "ou=users,o=my-organization,dc=my-domain,dc=com"
-   #:bind_method  :simple
+   def started
+     #TODO: If vault, check the schema to make sure that the
+association_attribute is there
+     @connections = []
+     @connection_index = 0
+   end
+
+
+
+
+   def each_entry
+     Net::LDAP.open(:host=>host, :port=>port, :auth=>auth) do |ldap|
+       ldap.search :base => search_base, :filter => search_filter,
+:return_result => false do |ldap_entry|
+         yield ldap_entry.dn, to_entry(ldap_entry)
+       end
+     end
+   end
+
+   # Runs the query specified by the config, gets the objectclass of the first
+   # returned object and returns a list of its allowed attributes
+   def self.fields
+     log.warn "Fields method not yet implemented for LDAP - Sorry."
+     log.warn "Returning a likely sample set."
+     %w{ cn givenName sn }
+   end
+
+
+
+   def self.sample_config
+     return <<END
+
+  host           'localhost'
+  port            389
+  username       'cn=Manager,dc=my-domain,dc=com'
+  password       'secret'
+  search_filter  "cn=*"
+  search_base    "ou=users,o=my-organization,dc=my-domain,dc=com"
+  #:bind_method  :simple
 END
-    end
+   end
 
 
 
-    def add(path, operations)
-      result = nil
-      with_ldap do |ldap|
-        attributes = perform_operations(operations)
-        result = ldap.add :dn=>path, :attributes=>attributes
-      end
-      log.debug("ldap.add returned '#{result}'")
-      return true
-    rescue Exception
-      log.warn "Exception occurred while adding LDAP record"
-      log.debug $!
-      false
-    end
+   def add(path, operations)
+     result = nil
+     with_ldap do |ldap|
+       attributes = perform_operations(operations)
+       result = ldap.add :dn=>path, :attributes=>attributes
+     end
+     log.debug("ldap.add returned '#{result}'")
+     return true
+   rescue Exception
+     log.warn "Exception occurred while adding LDAP record"
+     log.debug $!
+     false
+   end
 
-    def modify(path, operations)
-      log.debug "Modifying #{path} with the following operations:\n#{operations.inspect}"
-      with_ldap {|ldap| ldap.modify :dn=>path, :operations=>to_ldap_operations(operations) }
-    end
+   def modify(path, operations)
+     log.debug "Modifying #{path} with the following
+operations:\n#{operations.inspect}"
+     with_ldap {|ldap| ldap.modify :dn=>path,
+:operations=>to_ldap_operations(operations) }
+   end
 
-    def delete(path)
-      with_ldap {|ldap| ldap.delete :dn=>path }
-    end
+   def delete(path)
+     with_ldap {|ldap| ldap.delete :dn=>path }
+   end
 
-    def [](path)
-      with_ldap do |ldap|
-        result = ldap.search :base=>path, :scope=>Net::LDAP::SearchScope_BaseObject, :filter=>'objectclass=*'
-        return nil if !result or result.size == 0
-        # todo: See if this can be shortened
-        answer = {}
-        result[0].attribute_names.each do |name|
-          answer[name.to_s] = result[0][name]
-        end
-        answer
-      end
-    end
-    
-    # Called by unit tests to inject data
-    def test_add id, details
-      details << RubySync::Operation.new(:add, "objectclass", ['inetOrgPerson'])
-      add id, details
-    end
-    
-    def target_transform event
-      #event.add_default 'objectclass', 'inetOrgUser'
-      #is_vault? and event.add_value 'objectclass', RUBYSYNC_ASSOCIATION_CLASS
-    end
+   def [](path)
+     with_ldap do |ldap|
+       result = ldap.search :base=>path,
+:scope=>Net::LDAP::SearchScope_BaseObject, :filter=>'objectclass=*'
+       return nil if !result or result.size == 0
+       answer = {}
+       result[0].attribute_names.each do |name|
+         answer[name.to_s] = result[0][name]
+       end
+       answer
+     end
+   end
 
+   # Called by unit tests to inject data
+   def test_add id, details
+     details << RubySync::Operation.new(:add, "objectclass", ['inetOrgPerson'])
+     add id, details
+   end
 
-
-    private
-
-    def to_entry ldap_entry
-      entry = {}
-      ldap_entry.each do |name, values|
-        entry[name.to_s] = values.map {|v| String.new(v)}
-      end
-      entry
-    end
-
-    def operations_for_entry entry
-      ops = []
-      entry.each do |name, values|
-        ops << RubySync::Operation.add(name, values)
-      end
-      ops
-    end
-
-    
+   def target_transform event
+     #event.add_default 'objectclass', 'inetOrgUser'
+     #is_vault? and event.add_value 'objectclass', RUBYSYNC_ASSOCIATION_CLASS
+   end
 
 
 
-    def with_ldap
-      result = nil
-      Net::LDAP.open(:host=>host, :port=>port, :auth=>auth) do |ldap|
-        result = yield ldap
-      end
-      result
-    end
-    
-    def auth
-      {:method=>bind_method, :username=>username, :password=>password}
-    end
-    
-    # Produce an array of operation arrays suitable for the LDAP library
-    def to_ldap_operations operations
-      operations.map {|op| [op.type, op.subject, op.values]}
-    end
-    
+   private
+
+   def to_entry ldap_entry
+     entry = {}
+     ldap_entry.each do |name, values|
+       entry[name.to_s] = values.map {|v| String.new(v)}
+     end
+     entry
+   end
+
+   def operations_for_entry entry
+     ops = []
+     entry.each do |name, values|
+       ops << RubySync::Operation.add(name, values)
+     end
+     ops
+   end
+
+
+  def with_ldap
+     result = nil
+     @connections[@connection_index] = Net::LDAP.new(:host=>host,
+:port=>port, :auth=>auth) unless @connections[@connection_index]
+     if @connections[@connection_index]
+       ldap = @connections[@connection_index]
+       @connection_index += 1
+       result = yield ldap
+       @connection_index -= 1
+     end
+     result
   end
+
+
+   def auth
+     {:method=>bind_method, :username=>username, :password=>password}
+   end
+
+   # Produce an array of operation arrays suitable for the LDAP library
+   def to_ldap_operations operations
+     operations.map {|op| [op.type, op.subject, op.values]}
+   end
+
+ end
 end
