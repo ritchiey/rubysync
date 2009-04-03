4ca7ba46477d9d0b798c6f6550df94b950977031
Nowhere Man <nowhereman@open_office>
2009-04-03 10:39:07 +0200

    Update ActiveRecord Connector's test case (test_vault_to_client fails at the moment)

--- test/tc_active_record_connector.rb
+++ test/tc_active_record_connector.rb
@@ -20,7 +20,7 @@
 require 'ruby_sync_test'
 
 class ArActiveRecordConnector < RubySync::Connectors::ActiveRecordConnector
-    model :person
+  model :person
     application "#{File.dirname(__FILE__)}/../examples/ar_webapp"
    # dbm_path "/tmp/rubysync_ar_test"
 end
@@ -100,25 +100,26 @@ class TcActiveRecordConnector < Test::Unit::TestCase
   
   def test_vault_to_client
     # Turn on the RubySyncObserver to track the changes to people
-    #ActiveRecord::Base.observers = ::RubySyncObserver
-    # ::RubySyncObserver.observe ::Person
-    # ::RubySyncObserver.instance
-    # assert_nil ::RubySyncEvent.find(:first), "Pre-existing events in queue"
-    # person = Person.create :first_name=>"Ritchie", :last_name=>"Young"
-    # assert_not_nil ::RubySyncEvent.find_by_event_type('add'), "No add event generated"
-    # @pipeline.run_once
+      ActiveRecord::Base.observers = ::RubySyncObserver
+      ::RubySyncObserver.observe ::Person
+      ::RubySyncObserver.instance
+      assert_nil ::RubySyncEvent.find(:first), "Pre-existing events in queue"
+      person = ::Person.create :first_name=>"Ritchie", :last_name=>"Young"
+
+     assert_not_nil ::RubySyncEvent.find_by_event_type('add'), "No add event generated"
+     @pipeline.run_once
     # Find the association and use the key to look up the record on the client
-    #key = @vault.association_key_for @pipeline.association_context, person.id
-    #assert_not_nil key, "No association seems to have been created"
-    #c_person = @client.entry_for_own_association_key key
-    #assert_not_nil c_person, "Person wasn't created on client from vault; key='#{key}'\nClient contains:\n#{@client.inspect}"
-    #assert_equal "Ritchie", c_person['givenName'][0]
-    #assert_equal "Young", c_person['sn'][0]
-    # ActiveRecord::Base.observers = [] # Stop tracking changes to people
+    key = @vault.association_key_for @pipeline.association_context, person.id
+    assert_not_nil key, "No association seems to have been created"
+    c_person = @client.entry_for_own_association_key key
+    assert_not_nil c_person, "Person wasn't created on client from vault; key='#{key}'\nClient contains:\n#{@client.inspect}"
+    assert_equal "Ritchie", c_person['givenName'][0]
+    assert_equal "Young", c_person['sn'][0]
+     ActiveRecord::Base.observers = [] # Stop tracking changes to people
   end
   
   def find_bob
-    Person.find_by_first_name "bob"
+    ::Person.find_by_first_name "bob"
   end
   
   def test_fields
