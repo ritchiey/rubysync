c6b4a4f58f9afce2a72842b273311fc8f2647c7e
Nowhere Man <nowhereman@open_office>
2009-04-08 16:45:47 +0200

    Csv to Ldap example now working correctly with OpenLDAP and ApacheDS

--- examples/csv_to_ldap/config/connectors/ldap_vault_connector.rb
+++ examples/csv_to_ldap/config/connectors/ldap_vault_connector.rb
@@ -1,11 +1,30 @@
 class LdapVaultConnector < RubySync::Connectors::LdapConnector
-        
-   host           'localhost'
-   port            389
-   username       'cn=Manager,dc=my-domain,dc=com'
-   password       'secret'
-   search_filter  "cn=*"
-   search_base    "ou=users,o=my-organization,dc=my-domain,dc=com"
-   #:bind_method  :simple
+  
+  # ApacheDS config
+  host          'localhost'
+  port          10390
+  username      'uid=admin,ou=system'
+  password      'secret'
+  #changelog_dn  'cn=changelog'
+  search_filter "cn=*"
+  search_base   "ou=users,ou=system"
+
+  # OpenLDAP config
+#  host          'localhost'
+#  port          389
+#  username      'cn=admin,dc=localhost'
+#  password      'secret'
+#  #changelog_dn  'cn=changelog'
+#  search_filter 'cn=*'
+#  search_base   'dc=localhost'
+
+  # Default config
+#   host           'localhost'
+#   port            389
+#   username       'cn=Manager,dc=my-domain,dc=com'
+#   password       'secret'
+#   search_filter  "cn=*"
+#   search_base    "ou=users,o=my-organization,dc=my-domain,dc=com"
+#   #:bind_method  :simple
 
 end
