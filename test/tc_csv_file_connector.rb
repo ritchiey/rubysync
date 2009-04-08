8ed75151be4795f6bbebf4e28dcd70bad4e5c28b
Ritchie Young <ritchie@gmail.com>
2009-04-08 22:53:29 +0800

    Enabled CsvConnector to use headers in CSV files
    Signed-off-by: Babar O'Cap <babarocap@hotmail.com>

--- test/tc_csv_file_connector.rb
+++ test/tc_csv_file_connector.rb
@@ -49,8 +49,8 @@ class CsvTestPipeline < RubySync::Pipelines::BasePipeline
     map :givenName, 'given name'
     map :sn, 'last name'
     map :mail, :email
-  end
-  
+  end   
+    
 end
 
 class TcCsvConnector < Test::Unit::TestCase
@@ -65,6 +65,7 @@ class TcCsvConnector < Test::Unit::TestCase
     File.delete @filename if File.exists? @filename
     @pipeline.run_once # create the in and out directories if necessary
     @bob_details = {:cn=>'bob', :givenName=>"Robert", :sn=>"Smith", :mail=>'bob@thecure.com'}
+    @headers = {:cn=>'id', :givenName=>'given name', :sn=>'last name', :mail=>'email'}
   end
 
   
@@ -72,11 +73,9 @@ class TcCsvConnector < Test::Unit::TestCase
 #    CsvTestPipeline
 #  end
   
-  def test_client_to_vault
+  def client_to_vault_with
     banner :test_client_to_vault
-    CSV.open(@filename, 'w') do |csv|
-      csv << [:cn, :givenName, :sn, :mail].collect {|key| @bob_details[key]}
-    end
+    CSV.open(@filename, 'w') { |csv| yield csv }
     assert_nil @vault["bob"], "Vault already contains bob"
     @pipeline.run_once
     assert_not_nil @vault["bob"], "Bob wasn't created in vault"
@@ -84,5 +83,26 @@ class TcCsvConnector < Test::Unit::TestCase
     assert_equal modded_bob, @vault["bob"].reject {|k,v| ['modifier',:association].include? k}
     @pipeline.run_once
   end
+     
+  def test_client_to_vault_no_header
+    client_to_vault_with do |csv|
+      record = [:cn, :givenName, :sn, :mail].map {|key| @bob_details[key]}     
+      csv << record
+    end
+  end
+  
+  def test_client_to_vault_with_header
+    # When there's a header and field_names aren't specified,
+    # it should derive the field names from the header
+    def @client.header_line() true; end
+    def @client.field_names() nil; end
+
+    client_to_vault_with do |csv|   
+      keys = [ :givenName, :mail, :cn, :sn ]
+      csv << keys.map { |key| @headers[key] }
+      csv << keys.map { |key| @bob_details[key] }
+    end
+  end
+
 
 end
