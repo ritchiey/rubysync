852c6cdca78934ba5994d22b94db8e2003898dee
Nowhere Man <nowhereman@open_office>
2010-04-14 11:49:48 +0200

    Adding a new option "find_block" in ActiveRecordConnector, useful if you want to override the model name of your connector in a pipeline.
    If you have a UserConnectorlike this :
    
    class UserConnector < RubySync::Connectors::ActiveRecordConnector
      model :user # ActiveRecord model
    
      find_method :find_chaining
    
      find_chaining do |model, args|
        model.all(:conditions => "name IS NOT NULL")
      end
      ...
    end
    
    But in your Pipeline you want to used the SuperUser ActiveRecord model, you can do that :
    
    class MyPipeline < RubySync::Pipelines::BasePipeline
      client :user, :model => :super_user
      ...
    end

--- lib/ruby_sync/connectors/active_record_connector.rb
+++ lib/ruby_sync/connectors/active_record_connector.rb
@@ -4,13 +4,13 @@
 #  Copyright (c) 2009 Nowhere Man
 #
 # This file is part of RubySync.
-# 
+#
 # RubySync is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-# 
+#
 # RubySync is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along with RubySync; if not, write to the
 # Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
@@ -22,14 +22,14 @@ require "ruby_sync/connectors/base_connector"
 module RubySync::Connectors
 
   # You can initialize this connector with the name of a model and the path to a rails application:
-  # eg: vault :ActiveRecord, :application=>'path/to/rails/application', :model=>:user
+  # eg: vault :ActiveRecord, :application => 'path/to/rails/application', :model => :user
   class ActiveRecordConnector < RubySync::Connectors::BaseConnector
 
     include ActiveRecordAssociationTracking
     include ActiveRecordChangeTracking
-    
-    option :ar_class, :model, :changes_model, :associations_model, 
-      :application, :rails_env, :columns, :path_column, :find_method, :find_filter,
+
+    option :ar_class, :model, :changes_model, :associations_model,
+      :application, :rails_env, :columns, :path_column, :find_method, :find_filter, :find_block,
       :db_type, :db_host, :db_username, :db_password, :db_name, :db_encoding, :db_pool, :db_config
 
     rails_env 'development'
@@ -41,7 +41,7 @@ module RubySync::Connectors
     db_host 'localhost'
     db_name "rubysync_#{get_rails_env}"
     db_encoding "utf8"
-    db_pool 5    
+    db_pool 5
     # Default db_config in case we're not sucking the config out of a rails app
     db_config(
       :adapter=>get_db_type,
@@ -80,7 +80,10 @@ module RubySync::Connectors
     end
 
     def self.find_chaining(&block)
-      self.new.find_chaining(&block) if get_find_method == :find_chaining
+      if get_find_method == :find_chaining
+        find_block block
+        self.new.find_chaining(&block)
+      end
     end
 
     def find_chaining
@@ -88,15 +91,15 @@ module RubySync::Connectors
         meta_def :find_chaining do |*args|
           yield(self,args)
         end
-      end      
+      end
     end
 
     def initialize options={}
       super options
-     
+
       # Rails app specified, use it to configure
-      if application          
-        
+      if application
+
         # Load the database configuration
         rails_app_path = File.expand_path(application)
         ::Module.rails_app_path = rails_app_path
@@ -107,19 +110,19 @@ module RubySync::Connectors
         if new_db_config['adapter'].match('^(jdbc)?sqlite(2|3)?$')
           new_db_config['database'] = rails_app_path + '/' + new_db_config['database'] if Pathname.new(new_db_config['database']).relative?
         end
-        
+
         # Load db config
         self.class.db_config new_db_config
-       
+
         # Require the models
         @models||=[]
         Dir[File.join(rails_app_path, 'app','models', '*.rb')].each do |filepath|
           require_dependency filepath
           filepath = filepath.gsub("\\","/")#For Windows
           filename = File.basename(filepath,File.extname(filepath))
-          class_name = filename.camelize          
+          class_name = filename.camelize
           class_model = class_name.constantize
-          
+
           if(class_model.respond_to?(:descends_from_active_record?) && class_model.descends_from_active_record?)
             @models << class_name
             # Establish connection
@@ -129,7 +132,6 @@ module RubySync::Connectors
           end
         end
       end
-         
       self.class.ar_class model.to_s.camelize.constantize
       self.class.path_column ar_class.primary_key unless respond_to?(:path_column)
     end
@@ -147,7 +149,7 @@ module RubySync::Connectors
     # application. It will grab the config from the RoR database.yml.
     # application '/path/to/a/rails/application'
     # model 'name_of_model_to_sync'
-    # rails_env 'development'   # typically development or production 
+    # rails_env 'development'   # typically development or production
 
     # OR
 
@@ -161,23 +163,25 @@ module RubySync::Connectors
     # class MyModel < ActiveRecord::Base
     #     set_table_name "users"
     # end
-    
+
 END
     end
-      
-    
+
+
     def each_entry
       #      puts find_args.inspect#debug
+      find_chaining(&find_block) unless ar_class.respond_to?(find_method)
+
       ar_class.send(:"#{find_method}", *find_args).each do |record|
         yield record.send(:"#{path_column}"), to_entry(record)
       end
-    end     
+    end
 
     def find_args(extras=[])
       args = find_filter.is_a?(Array) ? find_filter : [find_filter]
       if respond_to?(:columns)
         columns << path_column.to_sym unless columns.include?(path_column.to_sym)
-        args << {:select => columns.join(", ")}      
+        args << {:select => columns.join(", ")}
       end
       args + extras
       args.merge_hashes
@@ -203,14 +207,14 @@ END
       return nil
     end
 
-      
+
     def modify(path, operations)
       ar_class.find(path) do |record|
         populate(record, perform_operations(operations))
         record.save
       end
     end
-    
+
     def delete(path)
       if path.is_a?(Hash)
         ar_class.destroy_all path[:conditions] if path.key?(:conditions)
@@ -224,7 +228,7 @@ END
     rescue ActiveRecord::RecordNotFound
       return nil
     end
-    
+
     #    def Object.const_missing(name)
     #      if name == :RAILS_ROOT
     #        File.expand_path(application)
